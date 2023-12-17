5bcf1db0b3596cb739f8adfaedc03c4c0425dd57
joseantoniorbieshna <jaramosb05@iesarroyoharnina.es>
2023-12-17 15:55:30 +0100

    Limitado con if

--- LibreriaJoseAntonio/Controllers/LibroController.cs
+++ LibreriaJoseAntonio/Controllers/LibroController.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
+using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
 using LibreriaJose.Models.Data;
